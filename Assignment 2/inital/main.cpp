32358bb8ec8081de5d021b0bb0bde86dd80f7ec7
lethanhbinhxq <binh.lethanh@hcmut.edu.vn>
2025-05-17 18:41:11 +0700

    Pass test configuration

--- Assignment 2/inital/main.cpp
+++ Assignment 2/inital/main.cpp
@@ -1,133 +1,51 @@
 /*
-* Ho Chi Minh City University of Technology
-* Faculty of Computer Science and Engineering
-* Initial code for Assignment 2
-* Programming Fundamentals Spring 2025
-* Date: 02.02.2025
-*/
+ * KTLT2 - HK242
+ * file : main.cpp
+ * SRC template
+ * Author: Võ Tiến
+ * Link FB : https://www.facebook.com/Shiba.Vo.Tien
+ * Link Group : https://www.facebook.com/groups/khmt.ktmt.cse.bku
+ * Date: 25.02.2024
+ */
 
-//The library here is concretely set, students are not allowed to include any other libraries.
+#include "main.hpp"
+#include "unit_test_Configuration.hpp"
 
-#include "hcmcampaign.h"
+string TEST_CASE = "Configuration";
+int main(int argc, char *argv[])
+{
+  cout << "-------------------------------------------------------------"
+       << endl;
+  cout << GREEN << BOLD << TEST_CASE << " - KTLT2 - HK242 - VO TIEN" << endl;
+  cout << CYAN << BOLD << "Vo Tien : https://www.facebook.com/Shiba.Vo.Tien" << endl
+       << endl;
+  if (argc < 1)
+  {
+    printTestCase();
+    return 1;
+  }
 
-using namespace std;
-
-void g_satc_01() {
-    cout << "----- Sample Testcase 01 -----" << endl;
-    Configuration* config = new Configuration("config.txt");
-    cout << config->str() << endl;
-    delete config;
-}
-void g_satc_02() {
-    cout << "----- Sample Testcase 02 -----" << endl;
-    Vehicle tankOfLiber(5, 2, Position(1, 2), TANK);
-    Vehicle truckOfLiber(3, 1, Position(2, 2), TRUCK);
-    Infantry sniperOfLiber(5, 2, Position(1, 1), SNIPER);
-
-    Vehicle tankOfAr(5, 2, Position(3, 2), TANK);
-    Vehicle truckOfAr(3, 1, Position(3, 1), TRUCK);
-    Infantry sniperOfAr(5, 2, Position(3, 3), SNIPER);
-
-    Unit** unitArrayOfLiber = new Unit*[3];
-    unitArrayOfLiber[0] = &tankOfLiber;
-    unitArrayOfLiber[1] = &truckOfLiber;
-    unitArrayOfLiber[2] = &sniperOfLiber;
-
-    Unit** unitArrayOfAr = new Unit*[3];
-    unitArrayOfAr[0] = &tankOfAr;
-    unitArrayOfAr[1] = &truckOfAr;
-    unitArrayOfAr[2] = &sniperOfAr;
-
-    LiberationArmy* liberationArmy = new LiberationArmy(unitArrayOfLiber, 3, "LiberationArmy", 0);
-    cout << liberationArmy->str() << endl;
-    ARVN* arvn = new ARVN(unitArrayOfAr, 3, "ARVN", 0);
-    cout << arvn->str() << endl;
-
-    Army* enemyLiber = arvn;
-    liberationArmy->fight(enemyLiber, false);
-    Army* enemyAr = liberationArmy;
-    arvn->fight(enemyAr, false);
-
-    cout << "* After the fight" << endl;
-    cout << liberationArmy->str() << endl;
-    cout << arvn->str() << endl;
-
-    delete liberationArmy;
-    delete arvn;
-    delete[] unitArrayOfLiber;
-    delete[] unitArrayOfAr;
+  handleTestUnit(argc, argv);
+  cout << "-------------------------------------------------------------"
+       << endl;
 }
 
-void sample_tc_2() {
-    cout << "----- Sample Testcase 02 -----" << endl;
-    Vehicle tankOfLiber(20, 20, Position(1, 2), MORTAR);
-    Vehicle truckOfLiber(15, 10, Position(2, 2), TRUCK);
-    Infantry sniperOfLiber(10, 20, Position(1, 1), SNIPER);
-    cout << tankOfLiber.str() << endl;
-    cout << truckOfLiber.str() << endl;
-    cout << sniperOfLiber.str() << endl;
+void handleTestUnit(int argc, char *argv[])
+{
+  UNIT_TEST_Configuration unitTest;
 
-    Vehicle tankOfAr(5, 20, Position(3, 2), MORTAR);
-    Vehicle truckOfAr(1, 10, Position(3, 1), TRUCK);
-    Infantry sniperOfAr(7, 20, Position(3, 3), SNIPER);
-
-    Unit** unitArrayOfLiber = new Unit*[3];
-    unitArrayOfLiber[0] = &tankOfLiber;
-    unitArrayOfLiber[1] = &truckOfLiber;
-    unitArrayOfLiber[2] = &sniperOfLiber;
-
-    Unit** unitArrayOfAr = new Unit*[3];
-    unitArrayOfAr[0] = &tankOfAr;
-    unitArrayOfAr[1] = &truckOfAr;
-    unitArrayOfAr[2] = &sniperOfAr;
-
-    LiberationArmy* liberationArmy = new LiberationArmy(unitArrayOfLiber, 3, "LiberationArmy", 0);
-    cout << liberationArmy->str() << endl;
-    ARVN* arvn = new ARVN(unitArrayOfAr, 3, "ARVN", 0);
-    cout << arvn->str() << endl;
-
-    Army* enemyLiber = arvn;
-    liberationArmy->fight(enemyLiber, false);
-    Army* enemyAr = liberationArmy;
-    arvn->fight(enemyAr, false);
-
-    cout << "* After the fight" << endl;
-    cout << liberationArmy->str() << endl;
-    cout << arvn->str() << endl;
-
-    delete liberationArmy;
-    delete arvn;
-    delete[] unitArrayOfLiber;
-    delete[] unitArrayOfAr;
-}
-
-void sample_tc_3() {
-    cout << "----- Sample Testcase 03 -----" << endl;
-    HCMCampaign* campaign = new HCMCampaign("config.txt");
-    cout << "** Before the fight" << endl;
-    cout << campaign->printResult() << endl;
-    cout << "** After the fight" << endl;
-    campaign->run();
-    cout << campaign->printResult() << endl;
-    delete campaign;
-}
-
-void sample_tc_7() {
-    cout << "----- Sample Testcase 07 -----" << endl;
-    Vehicle tankOfLiber1(20, 20, Position(1, 2), MORTAR);
-    Vehicle tankOfLiber2(5, 20, Position(3, 2), MORTAR);
-    Infantry sniperOfLiber1(10, 20, Position(1, 0), SNIPER);
-    Infantry sniperOfLiber2(10, 20, Position(1, 1), SNIPER);
-    Unit** unitArrayOfLiber8 = new Unit*[4];
-    unitArrayOfLiber8[0] = &tankOfLiber1;
-    unitArrayOfLiber8[1] = &tankOfLiber2;
-    unitArrayOfLiber8[2] = &sniperOfLiber1;
-    unitArrayOfLiber8[3] = &sniperOfLiber2;
-    LiberationArmy* liberationArmy8 = new LiberationArmy(unitArrayOfLiber8, 4, "LiberationArmy", 0);
-    cout << liberationArmy8->str() << endl;
-}
-
-int main(int argc, const char * argv[]) {
-    sample_tc_7();
-    return 0;
+  if (argc == 1 || (argc == 2 && std::string(argv[1]) == "all"))
+  {
+    std::cout << GREEN << BOLD << "Running unit_test/unit_test_" << TEST_CASE << RESET << "\n";
+    unitTest.runAllTests();
+  }
+  else if (argc == 2)
+  {
+    std::cout << GREEN << BOLD << "Running unit_test/unit_test_" << TEST_CASE << RESET << "\n";
+    unitTest.runTest(argv[1]);
+  }
+  else
+  {
+    printTestCase();
+  }
 }
